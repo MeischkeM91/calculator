c543f382a50c2a56d98be088d9b25c50a948017e
Max Meischke <meischkem@outlook.com>
2022-03-18 13:35:04 -0400

    ADded HTML boilerplate

--- script.js
+++ script.js
@@ -12,8 +12,3 @@ const divide = function(x, y){
     return x/y;
 }
 
-// Operate function will take 2 numbers and operand to call the correct math function
-const operator =function(num1,num2,oper){
-    this.num1=num1;
-    this.num2=num2;
-}
