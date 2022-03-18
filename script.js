07581ddbd5137d5b160cba39b850c47fa4d7af6b
Max Meischke <meischkem@outlook.com>
2022-03-18 13:59:50 -0400

    Created operate() and tested

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ const divide = function(x, y){
     return x/y;
 }
 
+// Operate function will take 2 numbers and operand to call the correct math function
+const operate =function(num1,num2,oper){
+    let operand=oper;
+    switch(operand){
+        case '+':
+            return add(num1,num2);
+            break;
+        case '-':
+            return subtract(num1,num2);
+            break;
+        case '*':
+            return multiply(num1,num2);
+            break;
+        case '/':
+            return divide(num1,num2);
+            break;
+        default:
+            console.log('::INVALID OPERAND::')
+    }
+}
+//test operate
+console.log(operate(20,4,'/'));
